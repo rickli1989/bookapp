7a16ae520a12635c2a723376fdbc8f47088788ac
Rick Li <rick_li_2@amp.com.au>
2019-02-25 10:00:43 +1100

    add book count selector showing navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { MatDialog } from '@angular/material';
 import { State } from './../store';
 import * as BookActions from './../store/book/book.actions';
+import { getBooksCount } from './../store/book/book.selectors';
 import { BookAddComponent } from './book-add/book-add.component';
 @Component({
   selector: 'app-root',
@@ -11,11 +12,12 @@ import { BookAddComponent } from './book-add/book-add.component';
 })
 export class AppComponent {
   title = 'BookApp';
-
+  count = 0;
   constructor(
     private store: Store<State>,
     private dialog: MatDialog
   ) {
+    this.readBooksCount();
   }
 
   openDialog(): void {
@@ -31,5 +33,14 @@ export class AppComponent {
       this.store.dispatch(action);
     });
   }
+
+  private readBooksCount() {
+    this.store.select(getBooksCount)
+      .subscribe(count => {
+        console.log(count);
+        this.count = count;
+      });
+  }
+
 
 }
---
+++ src/store/book/book.selectors.ts
@@ -0,0 +1,10 @@
+import { createSelector } from '@ngrx/store';
+import { State } from '../index';
+import { Book } from './book.model';
+
+export const getState = (state: State) => state;
+export const getBooks = (state: State) => state.books;
+
+export const getBooksCount = createSelector(getBooks, (books) => {
+  return books.length;
+});
